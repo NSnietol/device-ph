6a072efb23bbb891ecb96294a312491be691c082
Nilson Nieto López <nilson-sn@outlook.com>
2020-04-10 15:02:35 -0500

    [Add] Login for admins

--- exceptions/internet_exception.py
+++ exceptions/internet_exception.py
@@ -3,3 +3,9 @@ class NoInternetException(Exception):
 
     def __init__(self, message):
         self.message = message
+
+
+class WrongCredenciales(Exception):
+
+    def __init__(self, message):
+        self.message = message
