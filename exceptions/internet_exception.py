280d972523ab71a33c9b534717d2fe5d9197b52a
Nilson Nieto López <nilson-sn@outlook.com>
2020-04-24 21:28:23 -0500

    [add] new internet exception

--- exceptions/internet_exception.py
+++ exceptions/internet_exception.py
@@ -5,11 +5,16 @@ class NoInternetException(Exception):
         self.message = message
     def __str__(self):
         return self.message
-
-
 class WrongCredenciales(Exception):
 
     def __init__(self, message='Ha proporcionado datos incorrectos para acceder'):
         self.message = message
+    def __str__(self):
+        return self.message
+
+class BadResponse(Exception):
+    
+    def __init__(self, message='Error al solicitar recurso',url='url is missing'):
+        self.message = message +"\n"+ url
     def __str__(self):
         return self.message
